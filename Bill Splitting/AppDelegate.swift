5f56d88ff98563ef6959bccc305dceec6103d7a5
linas-ios <111570883+linas-ios@users.noreply.github.com>
2022-12-08 19:52:57 +0100

    Create Project

---
+++ Bill Splitting/AppDelegate.swift
@@ -0,0 +1,30 @@
+//
+//  AppDelegate.swift
+//  Bill Splitting
+//
+//  Created by Linas Nutautas on 08/12/2022.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+  
+
+
+  func applicationDidFinishLaunching(_ aNotification: Notification) {
+    // Insert code here to initialize your application
+  }
+
+  func applicationWillTerminate(_ aNotification: Notification) {
+    // Insert code here to tear down your application
+  }
+
+  func applicationSupportsSecureRestorableState(_ app: NSApplication) -> Bool {
+    return true
+  }
+
+
+}
+
