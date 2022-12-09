f72a750be9c76c63cb6f4062e408d59d2f4098e6
linas-ios <111570883+linas-ios@users.noreply.github.com>
2022-12-09 16:45:48 +0100

    Rename ViewController to CalculatorViewController

--- Bill Splitting/CalculatorViewController.swift
+++ Bill Splitting/CalculatorViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class CalculatorViewController: UIViewController {
   
   //MARK: Outlets
   @IBOutlet weak var enterAmountTextField: UITextField!
